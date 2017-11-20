23e1eae8ffc3bc93aacaf9cca02a77ad83bec0a8
Jens Pfeifer <jens.pfeifer@znuny.com>
2017-11-20 14:37:01 +0000

    Migrated to new article action settings.

---
+++ Kernel/Output/HTML/ArticleAction/MarkArticleSeenUnseen.pm
@@ -0,0 +1,88 @@
+# --
+# Copyright (C) 2012-2017 Znuny GmbH, http://znuny.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Output::HTML::ArticleAction::MarkArticleSeenUnseen;
+
+use strict;
+use warnings;
+
+use Kernel::Language qw(Translatable);
+
+our @ObjectDependencies = (
+    'Kernel::Config',
+    'Kernel::System::Log',
+    'Kernel::System::Ticket',
+);
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    my $Self = {};
+    bless( $Self, $Type );
+
+    return $Self;
+}
+
+sub CheckAccess {
+    my ( $Self, %Param ) = @_;
+
+    my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
+    my $LogObject    = $Kernel::OM->Get('Kernel::System::Log');
+    my $TicketObject = $Kernel::OM->Get('Kernel::System::Ticket');
+
+    # Check needed stuff.
+    for my $Needed (qw(Ticket Article ChannelName UserID)) {
+        if ( !$Param{$Needed} ) {
+            $LogObject->Log(
+                Priority => 'error',
+                Message  => "Need $Needed!",
+            );
+            return;
+        }
+    }
+
+    return if !$ConfigObject->Get('Frontend::Module')->{AgentTicketMarkSeenUnseen};
+
+    my $TicketPermission = $TicketObject->TicketPermission(
+        Type     => 'ro',
+        TicketID => $Param{Ticket}->{TicketID},
+        UserID   => $Param{UserID},
+        LogNo    => 1,
+    );
+    return if !$TicketPermission;
+
+    return 1;
+}
+
+sub GetConfig {
+    my ( $Self, %Param ) = @_;
+
+    my $LogObject = $Kernel::OM->Get('Kernel::System::Log');
+
+    for my $Needed (qw(Ticket Article UserID)) {
+        if ( !$Param{$Needed} ) {
+            $LogObject->Log(
+                Priority => 'error',
+                Message  => "Need $Needed!",
+            );
+            return;
+        }
+    }
+
+    my %MenuItem = (
+        ItemType    => 'Link',
+        Description => Translatable('Mark article as unseen'),
+        Name        => Translatable('Mark as unseen'),
+        Link =>
+            "Action=AgentTicketMarkSeenUnseen;Subaction=Unseen;TicketID=$Param{Ticket}->{TicketID};ArticleID=$Param{Article}->{ArticleID}",
+    );
+
+    return ( \%MenuItem );
+}
+
+1;
